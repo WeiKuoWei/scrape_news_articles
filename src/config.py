e4ba5b63e35bc2a7459e16dbea66e6be56d3bd46
WeiKuoWei <ck3294@nyu.edu>
2025-03-19 13:45:43 +0400

    chore: minor changes

--- src/config.py
+++ src/config.py
@@ -44,7 +44,7 @@ class ScraperConfig:
                 self.sites = json.load(f)
             
             # Only keep CNN and Fox News
-            self.target_sites = ['cnn']
+            self.target_sites = ['cnn', 'foxnews']
             self.sites = {k: v for k, v in self.sites.items() if k in self.target_sites}
             
             # Create data directories for each site
